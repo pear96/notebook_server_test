4a6a48ef2998aeaf1150f7d272fb760b9f40b2b3
pear96 <bhaeun1105@gmail.com>
2023-03-28 20:55:05 +0900

    initial commit

---
+++ src/main/java/study/homeserver/HomeserverApplication.java
@@ -0,0 +1,13 @@
+package study.homeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/homeserver/HomeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package study.homeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
